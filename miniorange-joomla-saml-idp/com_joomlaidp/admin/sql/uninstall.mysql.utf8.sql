2a7998e48bf3b137a2f553d646bc1656ed77dd81
miniOrange-joomla <215139780+miniOrange-joomla@users.noreply.github.com>
2025-12-29 22:49:14 +0530

    Delete miniorange-joomla-saml-idp directory

--- miniorange-joomla-saml-idp/com_joomlaidp/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__miniorangesamlidp`;
-DROP TABLE IF EXISTS `#__miniorange_saml_idp_customer`;
-ALTER TABLE `#__users` DROP COLUMN `userIn`;
-
-DROP TABLE IF EXISTS `#__mo_idp_logs`;
